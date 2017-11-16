2a2ff807d9970ccd0cb07a828ea62cbef049fdfa
Roy <technicmk@gmail.com>
2017-11-16 16:55:58 +0200

    Playing around with tests

--- tests/first.test.js
+++ tests/first.test.js
@@ -1,6 +1,17 @@
-const test = require('tape')
+import test from 'tape'
+import request from 'supertest'
+import app from '../src/routers'
 
-test('Things work', (assert) => {
-	assert.equal(1, 1)
-	assert.end()
-})
+test('Correct users returned', function (t) {
+  request(app)
+    .get('/api/users')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end(function (err, res) {
+      var expectedUsers = ['John', 'Betty', 'Hal'];
+
+      t.error(err, 'No error');
+      t.same(res.body, expectedUsers, 'Users as expected');
+      t.end();
+    });
+});
