6171baa4a795cb35cb1a760a286fc1495741d16c
Roy <technicmk@gmail.com>
2017-11-16 16:53:07 +0200

    Move app to routers/index.js

--- src/index.js
+++ src/index.js
@@ -1,44 +1,14 @@
-import express from 'express'
-import {graphiqlExpress} from 'apollo-server-express'
-import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 
-import models from './models'
-import makeAuthRouter from './routers/auth'
-import makeGraphqlRouter from './routers/graphql'
 import configureMongoose from './mongoose.config'
+import app from './routers'
 
 configureMongoose(mongoose)
 
-//cors middleware
-const cors = (req, res, next) => {
-	res.set('Access-Control-Allow-Origin', '*')
-	res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-	res.set('Content-Type', 'application/json')
-	if(req.method === 'OPTIONS')
-		res.sendStatus(200)
-	else
-		next()
-}
-
-const app = express()
-
-// attach middleware
-app.use(cors)
-app.use(bodyParser.json())
-
-//attach authentication router
-app.use('/auth', makeAuthRouter(models))
-
-//attach graphql router
-app.use('/graphql', makeGraphqlRouter(models))
-
-//attach graphiql
-app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
-
-// start listening
 const port = parseInt(process.env.PORT) || 5000
 const host = process.env.HOST || '0.0.0.0'
+
+// start listening
 app.listen(port, host, () => {
 	console.log(`Listening on ${host}:${port}`)
 })
---
+++ src/routers/index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import {graphiqlExpress} from 'apollo-server-express'
+import bodyParser from 'body-parser'
+
+import models from '../models'
+import makeAuthRouter from './auth'
+import makeGraphqlRouter from './graphql'
+
+//cors middleware
+const cors = (req, res, next) => {
+	res.set('Access-Control-Allow-Origin', '*')
+	res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+	res.set('Content-Type', 'application/json')
+
+	if(req.method === 'OPTIONS') return res.sendStatus(200)
+	next()
+}
+
+const app = express()
+
+// attach middleware
+app.use(cors)
+app.use(bodyParser.json())
+
+//attach authentication router
+app.use('/auth', makeAuthRouter(models))
+
+//attach graphql router
+app.use('/graphql', makeGraphqlRouter(models))
+
+//attach graphiql
+app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
+
+export default app
